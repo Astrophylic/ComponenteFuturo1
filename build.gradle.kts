21221aea6b51c380cba69077de7735ccd00516a3
Andrea <andrea0709vergara@gmail.com>
2026-01-15 10:36:14 +0100

    Cambios realizados.

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,6 @@ javafx {
 }
 
 dependencies {
-    // JUnit 5 completo
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
